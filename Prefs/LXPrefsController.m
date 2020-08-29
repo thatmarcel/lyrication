9f892e3a6d936a5d9efa94fea06836ff75b31cd4
Marcel Braun <marcel@strg.cc>
2020-08-29 22:36:27 +0200

    Start working on prefs

---
+++ Prefs/LXPrefsController.m
@@ -0,0 +1,29 @@
+#import "LXPrefsController.h"
+
+@implementation LXPrefsController
+	+ (nullable NSString *) hb_specifierPlist {
+		return @"Root";
+	}
+
+	- (instancetype) init {
+    	self = [super init];
+
+    	if (self) {
+        	UIBarButtonItem *respringItem = [[UIBarButtonItem alloc]
+												initWithTitle:@"Respring"
+											 	style:UIBarButtonItemStylePlain
+												target:self
+												action:@selector(respring:)];
+			self.navigationItem.rightBarButtonItem = respringItem;
+    	}
+
+    	return self;
+	}
+
+	- (void) respring:(id)sender {
+		pid_t pid;
+		const char *args[] = {"sbreload", NULL};
+		posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)args, NULL);
+	}
+
+@end
